2c04a796f8fd25ab7f6129504101e36e3a8f1ec5
shubnam <shubnam@DESKTOP-5SF9A7P>
2022-07-13 13:08:31 +0530

    first commit

---
+++ Assignment-1 Operators Decision Making and Control Structure/src/com/cg/looping/Program1_do_while.java
@@ -0,0 +1,16 @@
+package com.cg.looping;
+
+public class Program1_do_while {
+
+	public static void main(String[] args)
+	{
+		int x=11;
+		do 
+		{
+			System.out.println("Value of x is: " +x);
+			x++;
+		}while (x<20);
+      
+	}
+
+}
---
+++ Assignment-1 Operators Decision Making and Control Structure/src/com/cg/looping/Program2_Enhanced_for_loop.java
@@ -0,0 +1,15 @@
+package com.cg.looping;
+
+public class Program2_Enhanced_for_loop {
+
+	public static void main(String[] args) 
+	{
+		int array[]= {12,56,78,3,99,40};
+		for(int x:array)
+		{
+			System.out.println(x);
+		}
+		
+	}
+
+}
