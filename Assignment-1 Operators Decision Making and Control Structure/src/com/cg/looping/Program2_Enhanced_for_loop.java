06adcf72cf02e6d2e4842eeb5960e7990054df35
shubnam <shubnam@DESKTOP-5SF9A7P>
2022-07-14 17:51:38 +0530

    First commit of Day3 programs

--- Assignment-1 Operators Decision Making and Control Structure/src/com/cg/looping/Program2_Enhanced_for_loop.java
+++ Assignment-1 Operators Decision Making and Control Structure/src/com/cg/looping/Program2_Enhanced_for_loop.java
@@ -1,15 +1,26 @@
 package com.cg.looping;
 
+import java.util.Scanner;
+
 public class Program2_Enhanced_for_loop {
 
 	public static void main(String[] args) 
 	{
-		int array[]= {12,56,78,3,99,40};
-		for(int x:array)
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int arr[]=new int[n];
+		//to enter the array elements
+		for(int i=0;i<n;i++)
 		{
-			System.out.println(x);
+			arr[i]=s.nextInt();
 		}
-		
+		//to print the array elements
+		for(int i:arr)
+		{
+			System.out.println(i);
+			
+		}
+        s.close();
 	}
 
 }
